1a1adedc3704604d372a46d4532880f5c8b12e4c
histefan <hi.stefan1410@gmail.com>
2023-12-19 21:02:42 +0100

    gds fix

--- src/tt_um_histefan_top.v
+++ src/tt_um_histefan_top.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_histefan_top #( parameter MAX_COUNT = 24'd10_000_000 ) (
+module tt_um_histefan_top (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
@@ -12,11 +12,13 @@ module tt_um_histefan_top #( parameter MAX_COUNT = 24'd10_000_000 ) (
 );
 
     wire reset = ! rst_n;    
-    /*
 
     // use bidirectionals as outputs
     assign uio_oe = 8'b11111111;
-    */
+    assign uio_out = 8'b00000000;
+    
+    assign uo_out[2:0] = 3'b000;
+
     
     
 snake snake_game (
